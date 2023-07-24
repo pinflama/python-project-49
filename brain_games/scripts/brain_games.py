1ad5bf0512fbaac92ccfc01e565b46dba4859f4d
Flamingo Pink <pinflama@gmail.com>
2023-07-24 15:32:36 +0100

    step 2

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+def main():
+    print('Welcome to the Brain Games!')
+
+if __name__ == '__main__':
+    main()
+
