37a66ded8276d26561989c9d04ae95d6883c093a
Flamingo Pink <pinflama@gmail.com>
2023-07-28 06:55:22 +0100

    step 4

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,9 +2,10 @@
 
 import prompt
 
+
 def welcome_user():
-	print('Welcome to the Brain Games!')
-	name = prompt.string('May I have your name? ')
-	welcome_message = 'Hello, {}!'.format(name)
-	print(welcome_message)
-	return name
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    welcome_message = 'Hello, {}!'.format(name)
+    print(welcome_message)
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,9 +4,8 @@ from brain_games.cli import welcome_user
 
 
 def main():
-	welcome_user()
+    welcome_user()
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
